b65b246819a7a10d566bf01e643ac0bdbb434d8f
Valence Kajuna <valencekajuna@Valences-Macbook-Pro.local>
2020-12-01 16:13:26 +0300

    Hidding the toolbar for smaller screens
    Resolves #13

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const toolbar = () =>(
                 <Logo />
          </div>
          
-         <nav> <NavigationItems /> </nav>
+         <nav className={classes.DesktopOnly}> <NavigationItems /> </nav>
     </header>
 
 );
