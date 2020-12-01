54a8a9196c290df79d08403065573254ff7575c8
Valence Kajuna <valencekajuna@Valences-Macbook-Pro.local>
2020-12-01 17:24:33 +0300

    adding responsive backdrop
    Resolver #15

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../Navigation items/NavigationItems'; 
 import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../hoc/Aux';
+
 
 const SideDrawer = (props) => {
-    //...
+    let attachedClasses = [classes.SideDrawer, classes.Closed];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return(
-        <div className={classes.SideDrawer}> 
-            <div className={classes.Logo}>
-                 <Logo/>
-            </div>
+        <Aux>
+            <BackDrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}> 
+                <div className={classes.Logo}>
+                     <Logo/>
+                </div>
             
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
+        
     );
 }
 
