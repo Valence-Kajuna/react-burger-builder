86aa497ae30278705528e09771b6dafb74c65c22
Valence Kajuna <valencekajuna@Valences-Macbook-Pro.local>
2020-12-09 01:08:17 +0300

    added a custom input element component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
 import Order from '../../components/Order/Order';
-
+import axios from '../../axios-orders';
 class Orders extends Component{
+
+    state ={
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(res =>{
+            const fetchOrders =[];
+            for(let key in res.data){
+                fetchOrders.push({
+                    ...res.data[key],
+                id: key});
+            }
+            this.setState({loading:false, orders: fetchOrders})
+        }).catch((err) => {
+            this.setState({loading:false})
+        })
+    }
+    
 
     render(){
         return(
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map((order) => <Order key={order.key} price={order.price} ingredients={order.ingredients} />)}
             </div>
         );
     }
